1e1fd1c1cac323af0a9bb5941d91ac911422dfd9
Jeong Han Lee <jeonghan.lee@gmail.com>
2017-12-05 16:26:56 +0100

    update for real conf

--- setE3Env.bash
+++ setE3Env.bash
@@ -65,13 +65,13 @@ if [ -z "$base_ver" ]; then
     base_ver="3.15.5"
 fi
 
-# shared libs seach directory by require.c
-#
-# EPICS_DRIVER_PATH
 
-ESS_EPICS_PATH=/epics
+
+export ESS_EPICS_PATH=/testing/epics
+export EPICS=${ESS_EPICS_PATH}
+
 #ESS_LIBS=/opt/ess
-ESS_ETHERLAB=/opt/etherlab
+#ESS_ETHERLAB=/opt/etherlab
 #ESS_OPCUA=${ESS_LIBS}/opcUa
 #ESS_ETHERLAB=${ESS_LIBS}/etherlab
 
@@ -80,20 +80,23 @@ ESS_ETHERLAB=/opt/etherlab
 #export ESS_OPCUA_BIN=${ESS_OPCUA}/bin
 
 
-export ESS_ETHERLAB_LIB=${ESS_ETHERLAB}/lib
-export ESS_ETHERLAB_BIN=${ESS_ETHERLAB}/bin
-export ESS_ETHERLAB_SBIN=${ESS_ETHERLAB}/sbin
+# export ESS_ETHERLAB_LIB=${ESS_ETHERLAB}/lib
+# export ESS_ETHERLAB_BIN=${ESS_ETHERLAB}/bin
+# export ESS_ETHERLAB_SBIN=${ESS_ETHERLAB}/sbin
 
-export EPICS_LOCATION=${ESS_EPICS_PATH}/bases
-export EPICS_MODULES=${ESS_EPICS_PATH}/modules
-
-export EPICS=${EPICS_LOCATION}
 
 # shared libs seach directory by require.c
 #
 # EPICS_DRIVER_PATH
-export EPICS_DRIVER_PATH=${EPICS_MODULES}:${ESS_ETHERLAB_LIB}
+#export EPICS_DRIVER_PATH=${EPICS_MODULES}:${ESS_ETHERLAB_LIB}
 export EPICS_BASE=${EPICS}/base-${base_ver}
+
+
+
+# export EPICS_LOCATION=${ESS_EPICS_PATH}/bases
+# export EPICS_MODULES=${ESS_EPICS_PATH}/modules
+
+
 
 EPICS_HOST_ARCH=$("${EPICS_BASE}/startup/EpicsHostArch.pl")
 export EPICS_HOST_ARCH
@@ -102,13 +105,13 @@ export EPICS_HOST_ARCH
 
 export REQUIRE=require
 export REQUIRE_VERSION=${require_ver}
-export REQUIRE_PATH=${EPICS_MODULES}/${REQUIRE}/${REQUIRE_VERSION}
+export REQUIRE_PATH=${EPICS_BASE}/${REQUIRE}/${REQUIRE_VERSION}
 
 export REQUIRE_BIN=${REQUIRE_PATH}/bin
-export REQUIRE_LIB=${REQUIRE_PATH}/R${base_ver}/lib
-export REQUIRE_DBD=${REQUIRE_PATH}/R${base_ver}/dbd
+export REQUIRE_LIB=${REQUIRE_PATH}/lib
+export REQUIRE_DBD=${REQUIRE_PATH}/dbd
 
-export PATH=${REQUIRE_BIN}:${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin:${HOME}/bin:${ESS_ETHERLAB_BIN}:${ESS_ETHERLAB_SBIN}
+export PATH=${REQUIRE_BIN}:${EPICS_BASE}/bin/${EPICS_HOST_ARCH}:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/sbin:${HOME}/bin
 
-export LD_LIBRARY_PATH=${EPICS_BASE}/lib/${EPICS_HOST_ARCH}:${REQUIRE_LIB}/${EPICS_HOST_ARCH}:/usr/local/lib:${HOME}/lib:${ESS_ETHERLAB_LIB}
+export LD_LIBRARY_PATH=${EPICS_BASE}/lib/${EPICS_HOST_ARCH}:${REQUIRE_LIB}/${EPICS_HOST_ARCH}:/usr/local/lib:${HOME}/lib
 
